8e6298835fe5adde5dec7626eeebf3856277dd7a
JD Rich <jdrich@comcast.net>
2018-06-22 16:24:27 -0400

    Commiting the Settings class and run game methord

---
+++ Space Invaders/Settings.py
@@ -0,0 +1,17 @@
+class Settings(object):
+    """A class to store all of the setting for Alien Invasion"""
+    def __init__(self):
+        """initialize the games settings"""
+        #Screen settings
+        self.screenWidth = 1200
+        self.screenHeight = 800
+        self.bgColor = (0,230,0)
+        self.caption = "Alien Invasion"
+
+
+
+        
+
+
+
+
--- Space Invaders/Space_Invaders.py
+++ Space Invaders/Space_Invaders.py
@@ -1,4 +1,26 @@
 import sys
 import pygame
 
+from Settings import Settings
 
+def runGame():
+    #initialize pygame, screen and screen object
+    pygame.init()
+    aiSettings = Settings() #store the settings methods in the aiSettings to access them through this variable
+    screen = pygame.display.set_mode((aiSettings.screenWidth, aiSettings.screenHeight))
+    pygame.display.set_caption(aiSettings.caption)
+
+    #set the background color uses RGB
+    bgColor = (0,230,0)
+
+    #start the main loop for the game
+    while True:
+        #Watch for keyboard/mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        #Redraw the screen during each pass through the loop
+        screen.fill(aiSettings.bgColor)
+        #make the most recently drawn screen visible
+        pygame.display.flip()
+runGame()
