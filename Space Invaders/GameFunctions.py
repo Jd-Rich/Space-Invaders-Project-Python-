d264a55c671659640fd24f1117e800bc11d80aeb
JD Rich <jdrich@comcast.net>
2018-06-25 13:45:06 -0400

    Added updateScreen function to GameFunctions.py

--- Space Invaders/GameFunctions.py
+++ Space Invaders/GameFunctions.py
@@ -1,1 +1,17 @@
+import sys
 
+import pygame
+###Holds methods for Space Invaders###
+
+def checkEvents():
+    "Responds to keyclicks and keyboard input"
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def updateScreen(aiSettings, screen, ship):
+    """Update images on the screen and print to knew screen"""
+    screen.fill(aiSettings.bgColor)
+    ship.blitme()
+    #make the most recently drawn screen visible
+    pygame.display.flip()
--- Space Invaders/Space_Invaders.py
+++ Space Invaders/Space_Invaders.py
@@ -3,10 +3,11 @@ import pygame
 from Settings import Settings
 from Ship import Ship
 import GameFunctions as gf
+
 def runGame():
     #initialize pygame, screen and screen object
     pygame.init()
-    aiSettings = Settings() #store the settings methods in the aiSettings to access them through this variable
+    aiSettings = Settings() #store the settings variables in the aiSettings to access them through this variable
     screen = pygame.display.set_mode((aiSettings.screenWidth, aiSettings.screenHeight))
     pygame.display.set_caption(aiSettings.caption)
 
@@ -17,10 +18,6 @@ def runGame():
     while True:
         #Watch for keyboard/mouse events
         gf.checkEvents()
-        #Redraw the screen during each pass through the loop
-        screen.fill(aiSettings.bgColor)
-        ship.blitme()
+        gf.updateScreen(aiSettings, screen, ship)
 
-        #make the most recently drawn screen visible
-        pygame.display.flip()
 runGame()
